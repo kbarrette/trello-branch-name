b87fcbe157ec3c16fa448ee8687ebe23466074e5
Keith Barrette <etterrab@gmail.com>
2013-01-21 16:05:59 -0500

    Initial port of bookmarklet to extension, including copy to OS clipboard

---
+++ background.js
@@ -0,0 +1,25 @@
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  chrome.pageAction.show(tabId);
+});
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript(
+    tab.id,
+    {file: "jquery-1.9.0.min.js"},
+    function () { chrome.tabs.executeScript(tab.id, {file: "generateBranchName.js"}); }
+  );
+});
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+
+  // Copy to OS clipboard
+  if (request.action == "copy") {
+    $("#clipboard").val(request.value).select();
+    document.execCommand("copy");
+    $("#clipboard").val("");
+  }
+
+  sendResponse({});
+});
+
+
---
+++ generateBranchName.js
@@ -0,0 +1,37 @@
+(function($) {
+
+  var popover_selector = 'div.pop-over:visible';
+
+  function getBranchName() {
+    var hash = $(':contains("Link to this card") ~ input')[0].value.replace(/http.*\//, '');
+    var title = $('h2.window-title-text').text().toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '_');
+    var branch_name = title + "_tr_" + hash;
+
+    console.log(branch_name);
+    copy(branch_name);
+  }
+
+  function openPopover() {
+    $('a:contains("More")')[0].click();
+  }
+
+  function closePopover() {
+    $(popover_selector + " .js-close-popover")[0].click();
+  }
+
+  function copy(branch_name) {
+    chrome.extension.sendMessage({action: "copy", value: branch_name});
+  }
+
+  if (!$(popover_selector).length) {
+    openPopover();
+    setTimeout(function() {
+      getBranchName();
+      closePopover();
+    }, 1000);
+
+  } else {
+    getBranchName();
+  }
+
+})(jQuery);
